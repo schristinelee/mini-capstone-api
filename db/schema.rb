21905bbbebfc67dbcce87b3e6c4f0055e5b8411a
schristinelee <christinelee1527@gmail.com>
2022-11-09 10:30:48 -0800

    adds supplier table association

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_08_175437) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_09_182941) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.decimal "price", precision: 9, scale: 2
+    t.integer "price"
     t.string "image_url"
-    t.text "description"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "quantity"
+    t.integer "supplier_id"
   end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
