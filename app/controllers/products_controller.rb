c14ed20e3011626f8776b41cbded0f8626ea754c
schristinelee <christinelee1527@gmail.com>
2022-11-09 11:10:52 -0800

    adds jb files

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   def index
-    products = Product.all
-    render json: products.as_json
+    @products = Product.all
+    render index
   end
 
   def create
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,30 +1,28 @@
 class Product < ApplicationRecord
-  validates :name, presence: true
-  validates :name, uniqueness: true
-  validates :price, presence: true
-  validates :price, numericality: { greater_than: 0 }
-  validates :description, presence: true
-  validates :description, length: { in: 10..500 }
+  # validates :name, presence: true
+  # validates :name, uniqueness: true
+  # validates :price, presence: true
+  # validates :price, numericality: { greater_than: 0 }
+  # validates :description, presence: true
+  # validates :description, length: { in: 10..500 }
+
+  def supplier
+    Supplier.find_by(id: supplier_id)
+  end
 
   def friendly_created_at
     created_at.strftime("%B %e, %Y")
   end
 
   def is_discounted?
-    if price < 10
-      return true
-    else
-      return false
-    end
+    price <= 10
   end
 
   def tax
-    tax = price * 0.09
-    return tax
+    price * 0.09
   end
 
   def total
-    total = tax + price
-    return total
+    price + tax
   end
 end
